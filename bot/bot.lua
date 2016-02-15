27dc56cd9d4704df0296d49faadeabeef3c74e4f
focusworld <rezaabsy@yahoo.com>
2016-02-15 22:09:33 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-     sudo_users = {179286096,174602122,157187288,119650184,0,tonumber(our_id)},--Sudo users
+    sudo_users = {179286096,174602122,157187288,119650184,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
